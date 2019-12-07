3b1f279d5020f69d2d3b3b673c5112edc8184716
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2019-12-07 09:31:16 +0100

    More robust infinite load on scroll

--- _list/src/main/kotlin/list/presentation/EndlessLoadOnScrollListener.kt
+++ _list/src/main/kotlin/list/presentation/EndlessLoadOnScrollListener.kt
@@ -1,7 +1,6 @@
 package list.presentation
 
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 
 /**
@@ -9,38 +8,15 @@ import android.support.v7.widget.RecyclerView
  */
 internal abstract class EndlessLoadOnScrollListener(
     private val layoutManager: RecyclerView.LayoutManager) : RecyclerView.OnScrollListener() {
-  private var loading = true
-  private var previousTotalItemCount = 0
-
   override fun onScrolled(view: RecyclerView, dx: Int, dy: Int) {
-    val lastVisibleItemPosition = findLastVisibleItemPosition()
-    val totalItemCount = layoutManager.itemCount
-    if (loading && (totalItemCount > previousTotalItemCount)) {
-      loading = false
-      previousTotalItemCount = totalItemCount
-    }
-    if (!loading && lastVisibleItemPosition == totalItemCount - ITEM_THRESHOLD) {
-      loading = true
+    if ((layoutManager as GridLayoutManager).findLastVisibleItemPosition() ==
+        layoutManager.itemCount - 1 && dy > 0) {
       onLoadMore()
     }
   }
-
-  /**
-   * Independent of the layout manager in use.
-   */
-  private fun findLastVisibleItemPosition() =
-      when (layoutManager) {
-        is LinearLayoutManager -> layoutManager.findLastVisibleItemPosition()
-        is GridLayoutManager -> layoutManager.findLastVisibleItemPosition()
-        else -> throw IllegalStateException(
-            """Only ${LinearLayoutManager::class.java.name} or
-                    ${GridLayoutManager::class.java.name}""")
-      }
 
   /**
    * Implement refresh logic here.
    */
   protected abstract fun onLoadMore()
 }
-
-private const val ITEM_THRESHOLD = 1
