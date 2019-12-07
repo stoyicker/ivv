f109ac06e53ba28603c28a87256d278886e2f316
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2019-12-07 10:28:43 +0100

    Remove unnecessary scroll-to-top on invariant filter

--- _list/src/main/kotlin/list/presentation/Adapter.kt
+++ _list/src/main/kotlin/list/presentation/Adapter.kt
@@ -137,10 +137,13 @@ internal class Adapter(
     }
 
     override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+      val shouldScrollToTop = shownItems != results!!.values
       @Suppress("UNCHECKED_CAST")
       shownItems = results?.values as List<ListItem>? ?: items
       diff.dispatchUpdatesTo(this@Adapter)
-      recyclerView.scrollToPosition(0)
+      if (shouldScrollToTop) {
+        recyclerView.scrollToPosition(0)
+      }
     }
 
     /**
